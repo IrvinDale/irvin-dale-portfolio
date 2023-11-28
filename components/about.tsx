246bb9530fe965a3751ee35faf1a144011cadbbc
IrvinDale <vinzu24@gmail.com>
2023-11-28 10:25:45 -0500

    added about.tsx

---
+++ components/about.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb40 ">
+      <h2 className="text-3xl font-medium capitalize mb-8">About</h2>
+      <p className="mb-3">
+        After graduating with an advanced diploma in{" "}
+        <span className="font-medium">Software Engineering Technology</span>, I
+        decided to pursue my passion for programming. I continued learning{" "}
+        <span className="font-medium">Front-End Development </span>especially in{" "}
+        <span className="font-medium">React</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, React Native, Node.js, and Next.js
+        </span>
+        . I am also familiar with TypeScript and MySQL. I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p className="mb-3">
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies and anime, as well as playing the piano and
+        guitar. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I'm also
+        learning about digital arts.
+      </p>
+    </section>
+  );
+}
