a363e0eac4853401f0405f64e5335e77ee175a1c
IrvinDale <irvindale24@gmail.com>
2023-11-30 10:40:43 -0500

    added contact-form-email.tsx

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -3,6 +3,7 @@
 import React from "react";
 import { getErrorMessage, validateString } from "@/lib/utils";
 import { Resend } from "resend";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -29,7 +30,10 @@ export const sendEmail = async (formData: FormData) => {
       to: "vinzu24@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
-      text: message as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
     });
   } catch (error: unknown) {
     return {
