b328a815ff23b901c8e9154cd045dcc2f2033575
IrvinDale <irvindale24@gmail.com>
2023-11-30 19:24:54 -0500

    updated responsiveness

--- components/project.tsx
+++ components/project.tsx
@@ -32,13 +32,13 @@ export default function Project({
     >
       <section
         className=" bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden 
-    sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 group-even:even:pl-8
+    sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 sm:group-even:even:pl-8
     hover:bg-gray-200 transition
     dark:bg-white/10 dark:hover:bg-white/20 dark:text-white"
       >
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm-pr-2 sm:pt-10 sm:max-w-[50%] 
-      flex flex-col h-full group-even:ml-[18rem]"
+      flex flex-col h-full sm:group-even:ml-[18rem]"
         >
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p
@@ -65,7 +65,7 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
         transition
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
